ba6664d6023fb6cfb1767d69044cde150aa61177
joeyanhhuynh <joeyanhhuynh@yahoo.ca>
2015-02-27 02:53:16 -0500

    feat:(about)
    style about page, fix header, add search input show on hover

---
+++ _site/js/action.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.play').on('click', function(ev) {
+
+        $("#video")[0].src += "?autoplay=1";
+        ev.preventDefault();
+        $(this).hide();
+    });
+
+    jQuery(function ($) {
+
+        $('button.navbar-toggle').on('click', function () {
+            var $el = $(this),
+                textNode = this.lastChild;
+            $el.find('span').toggleClass('glyphicon-remove glyphicon-align-justify');
+        });
+    });
+    $(".back_top").click(function () {
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+
+    $(window).scroll(function (event) {
+        if ($(window).scrollTop() > 90) {
+            $('nav.navbar.navbar-default').addClass('scroll');
+        } else {
+            $('nav.navbar.navbar-default').removeClass('scroll');
+        }
+
+    });
+});
