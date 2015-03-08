0056a9dbe0be420fe5376eb787210031f2505c00
joeyanhhuynh <joeyanhhuynh@yahoo.ca>
2015-03-07 19:38:25 -0500

    feat
    responsive for home page

--- _site/js/action.js
+++ _site/js/action.js
@@ -72,4 +72,48 @@ $('.hand, .help_us_button').click(function(){
     });
 
 
+
+        $('#checkbox').change(function(){
+            setInterval(function () {
+                moveRight();
+            }, 3000);
+        });
+
+        var slideCount = $('#slider ul li').length;
+        var slideWidth = $('#slider ul li').width();
+        var slideHeight = $('#slider ul li').height();
+        var sliderUlWidth = slideCount * slideWidth;
+
+        $('#slider').css({ width: slideWidth, height: slideHeight });
+
+        $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+
+        $('#slider ul li:last-child').prependTo('#slider ul');
+
+        function moveLeft() {
+            $('#slider ul').animate({
+                left: + slideWidth
+            }, 200, function () {
+                $('#slider ul li:last-child').prependTo('#slider ul');
+                $('#slider ul').css('left', '');
+            });
+        };
+
+        function moveRight() {
+            $('#slider ul').animate({
+                left: - slideWidth
+            }, 200, function () {
+                $('#slider ul li:first-child').appendTo('#slider ul');
+                $('#slider ul').css('left', '');
+            });
+        };
+
+        $('div.control_prev').click(function () {
+            moveLeft();
+        });
+
+        $('div.control_next').click(function () {
+            moveRight();
+        });
+
 });
